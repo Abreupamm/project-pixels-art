05d18950b147723a86dc35387b42ee2d530b77ac
pamela <abreupamm@gmail.com>
2022-03-21 17:46:29 -0300

    desafio 5 feito

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function quadroPixels() {
     let quantElement = 5;
-    let totaElement = quantElement * quantElement;
     let sectionPai = document.getElementById('pixel-board');
-        for (let i = 1; i <= totaElement; i += 1) {
-            let quadro = document.createElement('div');
-            quadro.classList.add('pixel', 'white');
-            sectionPai.appendChild(quadro);
-        }
-    
+    for(let index = 1; index <= quantElement; index += 1){
+        let sect = document.createElement('div');
+    for (let i = 1; i <= quantElement; i += 1) {
+        let quadro = document.createElement('div');
+        quadro.classList.add('pixel', 'white');
+        sect.appendChild(quadro);
+    }
+    sectionPai.appendChild(sect);
+    }
 }
 quadroPixels();
+
