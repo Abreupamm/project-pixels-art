bf2ad6f4d4908ba404d55b3e660861a76ecd4913
pamela <abreupamm@gmail.com>
2022-03-21 18:43:29 -0300

    correção lint

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-function quadroPixels() {
-    let quantElement = 5;
-    let sectionPai = document.getElementById('pixel-board');
-    for(let index = 1; index <= quantElement; index += 1){
-        let sect = document.createElement('div');
+function quadroPixels(numero) {
+  const quantElement = numero;
+  const sectionPai = document.getElementById('pixel-board');
+  for (let index = 1; index <= quantElement; index += 1) {
+    const sect = document.createElement('div');
     for (let i = 1; i <= quantElement; i += 1) {
-        let quadro = document.createElement('div');
-        quadro.classList.add('pixel', 'white');
-        sect.appendChild(quadro);
+      const quadro = document.createElement('div');
+      quadro.classList.add('pixel', 'white');
+      sect.appendChild(quadro);
     }
     sectionPai.appendChild(sect);
-    }
+  }
 }
-quadroPixels();
-
+quadroPixels('5');
